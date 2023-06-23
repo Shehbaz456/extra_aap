c8f436c4d671a02b04978e7c68211f08d5ea1ffe
Shehbaz <106955650+Shehbaz456@users.noreply.github.com>
2023-06-23 13:50:34 +0530

    Add files via upload

---
+++ src/components/Spinner.js
@@ -0,0 +1,10 @@
+import loading from "./Spinner-3.gif"
+const Spinner=()=>{
+    return (
+      <div className='text-center' >
+        <img src={loading} alt="loading" />
+      </div>
+    )
+}
+
+export default Spinner
